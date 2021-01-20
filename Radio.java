0d05d5cdd05bcd66387e155084d3d9723a97c853
Ldsc2002 <luissantoscuellar@gmail.com>
2021-01-19 22:08:58 -0600

    Comentarios

--- Radio.java
+++ Radio.java
@@ -18,8 +18,8 @@ public class Radio implements Interface {
     private boolean emisora; // true = am | false = fm
     private boolean estado; // Guarda el estado del radio | true = encendido, false = apagado
     private double estacion; // Guarda la estacion de radio que se esta sintonizando
-    private double[] memoriaAM = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    private double[] memoriaFM = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    private double[] memoriaAM = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //Radios AM guardadas en la radio
+    private double[] memoriaFM = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //Radios FM guardadas en la radio
 
     //Cosntructor
     public Radio(){
@@ -28,65 +28,75 @@ public class Radio implements Interface {
     }
     
     @Override
-    public boolean encenderApagar() {
-        if (estado == false) {
-            estado = true;
+    public boolean encenderApagar() { //Cambia el estado de la radio
+        if (estado == false) { //Si esta apagada
+            estado = true; //La enciende
             return true;
         }
-        else {
-            estado = false;
+        else { //Si esta encendida
+            estado = false; //La apaga
             return false;
         }
+        //Devuelve el estado de la radio
     }
 
     @Override
-    public boolean amFm() {
-        if (emisora == false) {
-            emisora = true;
-            estacion = 530;
+    public boolean amFm() { //Cambia entre AM y FM
+        if (emisora == false) { //Si es FM
+            emisora = true; //Cambia a AM
+            estacion = 530; //Estacion inicial de AM
             return true;
         }
-        else {
-            emisora = false;
-            estacion = 87.9;
+        else { //Si es AM
+            emisora = false; //Cambia a FM
+            estacion = 87.9; //Estacion inicial de FM
             return false;
         }
+        //Devuelve si esta en AM o FM
     }
 
     @Override
-    public String guardar(int btn) {
-        if (emisora == true) {
-            memoriaAM[btn - 1] = estacion;
-        } else {    
-            memoriaFM[btn - 1] = estacion;
+    public String guardar(int btn) { //Guarda la estacion actual en el boton indicado
+        if (emisora == true) { //Si es AM
+            memoriaAM[btn - 1] = estacion; //Guarda la estacion AM
+        } else { //Si es FM
+            memoriaFM[btn - 1] = estacion; //Guarda la estacion FM
         }
 
-        return String.valueOf(estacion);
+        return String.valueOf(estacion); //Devuelve la estacion almacenada
     }
 
     @Override
-    public String seleccionar(int btn) {
-        if (emisora == true) {
-            estacion = memoriaAM[btn - 1];
-        } else {    
-            estacion = memoriaFM[btn - 1];
-        }
-        return String.valueOf(estacion);
-    }
-
-    @Override
-    public double avanzar() {
-        if (emisora == true) {
-            estacion = estacion + 10;
-            if (estacion == 1620) {
-                estacion = 530;
+    public String seleccionar(int btn) { //Selecciona una estacion previamente almacenada
+        if (emisora == true) { //Si es AM
+            if (memoriaAM[btn - 1] != 0) { //Verifica que haya una estacion guardada
+                estacion = memoriaAM[btn - 1]; //Cambia la estacion
+            } else { //Si no hay estacion guardada
+                return "";
             }
-        } else {
-            estacion = estacion + 0.2;
-            if (estacion == 108.1) {
-                estacion = 107.9;
+        } else { //Si es FM
+            if (memoriaFM[btn - 1] != 0) { //Verifica que haya una estacion guardada
+                estacion = memoriaFM[btn - 1]; //Cambia la estacion
+            } else { //Si no hay estacion guardada
+                return "";
             }
         }
-        return estacion;
+        return String.valueOf(estacion); //Devuelve la estacion a la que se cambio
+    }
+
+    @Override
+    public double avanzar() { //Avanza a la siguiente estacion 
+        if (emisora == true) { //Si es AM
+            estacion = estacion + 10; //Cambia a la siguiente estacion
+            if (estacion == 1620) { //Si llega al final de las frecuencias
+                estacion = 530; //Regresa al inicio
+            }
+        } else { //Si es FM
+            estacion = estacion + 0.2; //Cambia a la siguiente estacion
+            if (estacion == 108.1) { //Si llega al final de las frecuencias
+                estacion = 107.9; //Regresa al inicio
+            }
+        }
+        return estacion; //Regresa la estacion que se esta sintonizando
     }
 }
